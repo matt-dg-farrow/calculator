680bd642a2c4bc6100375688f61d643e0e07a898
Harr1s96 <harrishanley96@gmail.com>
2019-12-03 15:02:18 +0000

    pulled changes

---
+++ src/test/java/com/qa/app/AdditionTest.java
@@ -0,0 +1,18 @@
+// package com.qa.app;
+
+// import org.junit.*;
+
+// public class AdditionTest {
+
+// @Test
+// public void Test() {
+
+//     int x = 34;
+//     int y = 75;
+
+// }
+
+
+
+
+// }
