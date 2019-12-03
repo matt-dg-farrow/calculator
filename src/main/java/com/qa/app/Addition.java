5264bffacec9e75eb3e4945883ec6d4c7ca1ddce
Harr1s96 <harrishanley96@gmail.com>
2019-12-03 13:04:36 +0000

    just push

--- src/main/java/com/qa/app/Addition.java
+++ src/main/java/com/qa/app/Addition.java
@@ -2,39 +2,7 @@ package com.qa.app;
 
 public class Addition implements Operations {
 
-    private int number1;
-
-    private int number2;
-
-    /**
-     * @param number1 the number1 to set
-     */
-    public void setNumber1(int number1) {
-        this.number1 = number1;
-    }
-
-    /**
-     * @param number2 the number2 to set
-     */
-    public void setNumber2(int number2) {
-        this.number2 = number2;
-    }
-
-    /**
-     * @return the number1
-     */
-    public int getNumber1() {
-        return number1;
-    }
-
-    /**
-     * @return the number2
-     */
-    public int getNumber2() {
-        return number2;
-    }
-
-    public int calculate(int number1, int getNumber2) {
+    public int calculate(int number1, int number2) {
         
         int z;
 
